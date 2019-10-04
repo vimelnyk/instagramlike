37b23fcf877ebde8173648e59a12db2a6003297f
Vira Melnyk <vira_melnyk@epam.com>
2019-10-04 15:44:13 +0200

    Changes

--- public/src/index.js
+++ public/src/index.js
@@ -1,8 +1,10 @@
+let fullObject;
 fetch("http://localhost:3004/posts")
   .then(function(response) {
     return response.json();
   })
   .then(function(obj) {
+    fullObject = obj;
     console.log(JSON.stringify(obj));
     console.log(obj);
     for (let j = 0; j < obj.length; j++) {
@@ -12,3 +14,5 @@ fetch("http://localhost:3004/posts")
   .catch(function(error) {
     console.error("Error:", error);
   });
+
+console.log(JSON.stringify(obj));
